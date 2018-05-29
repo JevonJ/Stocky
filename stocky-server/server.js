d3da779e3ac925dddea5c092f0dd360081e4b857
Jevon Jans <jevoon1@gmail.com>
2018-05-29 16:37:42 +0530

    Add listener file Removed unnecessary code

--- stocky-server/server.js
+++ stocky-server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
+const listeners = require('./src/listeners');
 
 //Our local port
 const port = 4001;
@@ -10,46 +11,16 @@ const app = express();
 //our server instance
 const server = http.createServer(app);
 
+// Set public folder
+app.use(express.static('public'));
+
 //this creates a socker using the server instance
 const io = socketIO(server);
 
 io.on('connection', socket => {
     console.log('New User Connected');
 
-    socket.on('change color', (color) => {
-      console.log('Color Changed to: ', color);
-      io.sockets.emit('change color', color);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    });
+    listeners.listen(socket, io);
 });
 
 server.listen(port, ()=> console.log(`Listening on port ${port}`));
-
-
-// const express = require('express');
-// const path = require('path');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-
-// const app = express();
-
-// const poll = require('./routes/poll');
-
-// // Set public folder
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// // Body parser middleware
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-
-// // Enable Cors
-// app.use(cors());
-
-// app.use('/poll', poll);
-
-// const port = 3000;
-
-// app.listen(port, () => console.log(`Listening on port ${port}`));
