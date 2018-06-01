6bd829abb5bedb790dd91da2fbab6140218116c5
Jevon Jans <jevoon1@gmail.com>
2018-06-01 19:10:16 +0530

    Change styling format

--- stocky-client/src/components/login/Welcome.jsx
+++ stocky-client/src/components/login/Welcome.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Fade, Button } from 'reactstrap';
-import { withRouter } from 'react-router';
+import { withRouter } from 'react-router-dom';
+import './Welcome.css';
 
 const Welcome = withRouter(({ history }) => (
   <Fade in tag="div" timeout={300}>
-    <main role="main" className="inner cover">
-      <h1 className="cover-heading">Welcome to Stocky</h1>
+    <main role="main" className="inner loginWelcome-cover">
+      <h1 className="loginWelcome-cover-heading">Welcome to Stocky</h1>
       <p className="lead">Would you like to join or host a game ?</p>
       <p className="lead">
-      <Button outline color="primary" size="lg">Join Game</Button>
-        <Button outline color="primary" onClick={() => { history.push('/host-game') }} size="lg">Host Game</Button>
+        <Button outline color="primary" size="lg">Join Game</Button>
+        <Button outline color="primary" onClick={() => history.push('/host-game')} size="lg">Host Game</Button>
       </p>
     </main>
   </Fade>
