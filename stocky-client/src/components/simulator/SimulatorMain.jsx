a39be45412400a23df15ece67812fa452a2b1476
Inuri Jayanika <inufbi@gmail.com>
2018-06-03 06:38:55 +0530

    Initial implementation of simulator

--- stocky-client/src/components/simulator/SimulatorMain.jsx
+++ stocky-client/src/components/simulator/SimulatorMain.jsx
@@ -3,23 +3,21 @@ import { connect } from 'react-redux';
 import { Button, Fade, Popover, PopoverBody, PopoverHeader } from 'reactstrap';
 import './SimulatorMain.css';
 
-import Sidebar from './Sidebar';
+//import Sidebar from './Sidebar';
 import Header from './Header';
 import Dashboard from './Dashboard';
 
 class SimulatorMain extends Component {
   render() {
     return (
-    <div>
-      <Header />
-    <div class="container-fluid">
-      <div class="row">
-        <Sidebar />
-
-        <Dashboard />
+      <div>
+        <Header />
+        <div class="container-fluid">
+          <div class="p-3 mb-2 bg-gradient-dark text-black">
+            <Dashboard />
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
     );
   }
 }
