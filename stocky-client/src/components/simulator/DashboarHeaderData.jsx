198eb072456b2edb5c4698fbfd3ac50778855c00
Samadhi <samadhibuddhimali@gmail.com>
2018-06-20 08:17:36 +0530

    Add view to see remaining cash in hand

---
+++ stocky-client/src/components/simulator/DashboarHeaderData.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { Row, Col, ListGroup, ListGroupItem, Badge } from 'reactstrap';
+class DashboardHeaderData extends Component {
+   calculatePurchased(player) {
+    const{playerStocks}=this.props;
+    console.log("data",playerStocks);
+    const stocks = playerStocks[player.name].purchased;
+    const sum = stocks.reduce((total, stock) => {
+      if (stock.initStockQty === 0) {
+        return total;
+      }
+      const remainingStocks = (stock.initStockQty - stock.soldStockQty.reduce((a, b) => a + b, 0));
+
+      return total + (stock.unitPrice * remainingStocks);
+    }, 0);
+
+    return sum;
+  }
+
+  render() {
+    const {
+     user,
+    } = this.props;
+    const stockValue = this.calculatePurchased(user);
+    return (
+      <Row>
+        <Col>
+          <ListGroup>
+            <ListGroupItem
+              className="justify-content-between"
+              color="danger"
+            >
+              <h4>Cash Remaining</h4><h5><Badge pill>LKR {user.cash}</Badge></h5>
+            </ListGroupItem>
+          </ListGroup>
+        </Col>
+        <Col>
+          <ListGroup>
+            <ListGroupItem
+              className="justify-content-between"
+              color="warning"
+            >
+              <h4>Stock Value</h4><h5><Badge pill>LKR {stockValue}</Badge></h5>
+            </ListGroupItem>
+          </ListGroup>
+        </Col>
+        <Col>
+          <ListGroup>
+            <ListGroupItem
+              className="justify-content-between"
+              color="info"
+            >
+              <h4>Total Asset Value</h4><h5><Badge pill>LKR {user.cash + stockValue}</Badge></h5>
+            </ListGroupItem>
+          </ListGroup>
+        </Col></Row>
+    );
+  }
+}
+
+
+
+export default DashboardHeaderData;
