0b01454ebc2ebe197ca38efe2eef3aaf8350a25c
Inuri Jayanika <inufbi@gmail.com>
2018-06-23 12:43:20 +0530

    Add another two description lines.

--- stocky-client/src/components/login/HowToPlay.jsx
+++ stocky-client/src/components/login/HowToPlay.jsx
@@ -18,8 +18,10 @@ class HowToPlay extends Component {
           <li>Being the host for a game, Stocky allows you to create a game room (i.e. a match) and you can play with online players who are joining to your game room.</li><br/>
           <li>You can even create your private game room with a password so that the friends who sharing the password can enjoy the game with you!</li><br/>
           <li>If you not interest in hosting a game You can join to a game room which is not started playing yet.</li><br/>
+          <li>Hang on at least with three players to start a game.</li><br/>
           <li>Host of the game can decide the number of rounds they going to play in the game.</li><br/>
-          <li>We give 1000 LKR. to every player in the game at the starting point.</li><br/>
+          <li>Host of the game can decide how much time will it takes for a round in the game.</li>
+          <li>We give LKR.1000 to every player in the game at the starting point.</li><br/>
           <li>You can pay your attention on live feed and game room members asset levels and give them a competitive experience while you are enjoying the game.</li><br/>
           <li>Highest valued asset holder of all rounds in the game will be the WINNER OF THE GAME!!!</li><br/>
           <li>You can not pause the game after you start playing.</li><br/>
