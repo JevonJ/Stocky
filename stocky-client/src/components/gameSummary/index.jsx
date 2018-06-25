433fc041197dce1cb2e7374f9004154f936b43b5
tharaniya <tharaniyasiva@gmail.com>
2018-06-25 21:02:13 +0530

    game summary quit game #issue-94

--- stocky-client/src/components/gameSummary/index.jsx
+++ stocky-client/src/components/gameSummary/index.jsx
@@ -130,8 +130,8 @@ class GameSummary extends Component {
         </Row>
         <Row>
           <Col>
-            <Button outline color="success" size="lg">New Game</Button>
-            <Button outline color="danger" size="lg">Quit</Button>
+            <Button href="/" outline color="success" size="lg">New Game</Button>
+            <Button href="/" outline color="danger" size="lg">Quit</Button>
           </Col>
         </Row>
       </Container>
